1099095c40bb0aaccef2a2e16f3b64e06d9fbad7
anpham148 <anpham148@gmail.com>
2021-02-04 12:41:53 +0000

    commit tree 47b5fbd7edd1711c965417929c30768b63c20e59

--- src/index.js
+++ src/index.js
@@ -12,4 +12,74 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  
+  getToys();
+  
+  const name = document.querySelector('input[name="name"]').value;
+  const image = document.querySelector('input[name="image"]').value;
+  const submit = document.querySelector('input[name="image"]');
+
+  submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    addNewToy(name, image);
+  });
 });
+
+function getToys() {
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(json => {
+      json.forEach(toy => renderToy(toy))
+  })
+}
+
+function renderToy(toy) {
+   const collection = document.getElementById('toy-collection');
+   const div = document.createElement('div');
+   div.className = "card";
+   collection.appendChild(div);
+
+   const h2 = document.createElement('h2');
+   h2.innerHTML = toy['name']
+   div.appendChild(h2)
+
+   const img = document.createElement('img');
+   img.src = toy['image'];
+   img.className = 'toy-avatar';
+   div.appendChild(img);
+
+   const p = document.createElement('p');
+   p.innerHTML = `${toy['likes']} Likes`;
+   div.appendChild(p);
+
+   const btn = document.createElement("BUTTON");
+   btn.className = 'like-btn';
+   btn.innerHTML = 'Like <3';
+   div.appendChild(btn);
+}
+
+function addNewToy(name, image) {
+  let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+          "name": name,
+          "image": image,
+          "likes": 0
+      })
+    };
+     
+  fetch("http://localhost:3000/toys", configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        renderToy(object);
+      })
+      .catch(function(error) {
+          document.body.innerHTML = error.message;
+      });
+  }
